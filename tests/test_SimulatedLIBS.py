3ce76d6da0eb7fd5727ca2bb73ad5cb30193b94e
MKastek <mkastek8@gmail.com>
2022-09-29 18:04:07 +0200

    tests

--- tests/test_SimulatedLIBS.py
+++ tests/test_SimulatedLIBS.py
@@ -3,4 +3,4 @@ from SimulatedLIBS.simulation import SimulatedLIBS
 
 def test():
     libs = SimulatedLIBS(elements=["H"], percentages=[100])
-    assert libs.get_raw_spectrum() is not None
+    assert libs.get_raw_spectrum() is None
